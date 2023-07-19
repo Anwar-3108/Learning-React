c2e3a1e9f8aa25833e6ab8efc427c397694d15a4
Anwar-3108 <syed.sahab9628@gmail.com>
2023-07-19 16:20:51 +0530

    styled cards, passed data , passed props

--- Chapter-4/App.js
+++ Chapter-4/App.js
@@ -56,19 +56,177 @@ const HeaderComp = () => {
   );
 };
 
-const Body = () => {
+// config driven UI mean, the UI which  is deriven by configration.
+
+const config = [
+  {
+    type: "carousel",
+    cards: [
+      {
+        offerName: "50% off",
+      },
+      {
+        offerName: "No Delevery Charge",
+      },
+    ],
+  },
+  {
+    type: "restraunt",
+    cards: [
+      {
+        name: "Burger King",
+        image:
+          "https://res.cloudinary.com/purnesh/image/upload/w_1080,f_auto/butterfly-chicken-burger.jpg",
+
+        price: " Rs 250/-",
+        rating: "4.2",
+        cusines: ["Burgers ", " Amarican"],
+      },
+      {
+        name: " Veg Pizza",
+        image:
+          "https://imgmedia.lbb.in/media/2020/02/5e58fb96227dbe1b8e53ada4_1582889878493.png",
+
+        price: " Rs 450/-",
+        rating: "4.2",
+        cusines: ["Pizza ", " Indian"],
+      },
+      {
+        name: "West Thali",
+        image:
+          "https://articles-images.sftcdn.net/wp-content/uploads/sites/3/2019/08/1280-Indian-food.jpg",
+
+        price: " Rs 530/-",
+        rating: "5.2",
+        cusines: ["Thali ", " Indian", "West", "Veg and Non-Veg"],
+      },
+      {
+        name: "Idli Dosa",
+        image:
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/SouthIndian_a5ivgy",
+
+        price: " Rs 280/-",
+        rating: "4.7",
+        cusines: ["Idli and Dosa  ", " Indian", "Veg"],
+      },
+      {
+        name: "Dam Pulaw",
+        image:
+          "hhttps://images.livemint.com/img/2022/12/15/600x338/swiggy_biryani_1671108299094_1671108299222_1671108299222.jpg",
+
+        price: " Rs 699/-",
+        rating: "5.9",
+        cusines: ["Dam Pulaw ", " Indian", "Madrasi"],
+      },
+      {
+        name: "Biryani",
+        image:
+          "https://etimg.etb2bimg.com/thumb/msid-96285119,imgsize-58228,width-1200,height=765,overlay-etbrandequity/research/2022-saw-23-increase-in-searches-for-guilt-free-food-options-swiggy-report.jpg",
+
+        price: " Rs 650/-",
+        rating: "5.2",
+        cusines: ["Biryani ", " Indian"],
+      },
+    ],
+  },
+];
+
+const restrauntList = [
+  {
+    name: "Burger King",
+    image:
+      "https://res.cloudinary.com/purnesh/image/upload/w_1080,f_auto/butterfly-chicken-burger.jpg",
+
+    price: " Rs 250/-",
+    rating: "4.2",
+    cusines: ["Burgers ", " Amarican"],
+  },
+  {
+    name: " Veg Pizza",
+    image:
+      "https://imgmedia.lbb.in/media/2020/02/5e58fb96227dbe1b8e53ada4_1582889878493.png",
+
+    price: " Rs 450/-",
+    rating: "4.2",
+    cusines: ["Pizza ", " Indian"],
+  },
+  {
+    name: "West Thali",
+    image:
+      "https://articles-images.sftcdn.net/wp-content/uploads/sites/3/2019/08/1280-Indian-food.jpg",
+
+    price: " Rs 530/-",
+    rating: "5.2",
+    cusines: ["Thali ", " Indian", "West", "Veg and Non-Veg"],
+  },
+  {
+    name: "Idli Dosa",
+    image:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/SouthIndian_a5ivgy",
+
+    price: " Rs 280/-",
+    rating: "4.7",
+    cusines: ["Idli and Dosa  ", " Indian", "Veg"],
+  },
+  {
+    name: "Dam Pulaw",
+    image:
+      "https://etimg.etb2bimg.com/thumb/msid-96285119,imgsize-58228,width-1200,height=765,overlay-etbrandequity/research/2022-saw-23-increase-in-searches-for-guilt-free-food-options-swiggy-report.jpg",
+
+    price: " Rs 699/-",
+    rating: "5.9",
+    cusines: ["Dam Pulaw ", " Indian", "Madrasi"],
+  },
+  {
+    name: "Biryani",
+    image:
+      "https://etimg.etb2bimg.com/thumb/msid-96285119,imgsize-58228,width-1200,height=765,overlay-etbrandequity/research/2022-saw-23-increase-in-searches-for-guilt-free-food-options-swiggy-report.jpg",
+
+    price: " Rs 650/-",
+    rating: "5.2",
+    cusines: ["Biryani ", " Indian"],
+  },
+];
+
+const RestrauntCard = (props) => {
+  console.log(props);
   return (
     <>
-      <h3>Body</h3>
+      <div className="card">
+        <img src={props.restraunt?.image} alt="" />
+        <h1>{props.restraunt?.name}</h1>
+        <h2>{props.restraunt?.price}</h2>
+        <h3>{props.restraunt?.rating}</h3>
+        <h4>{props.restraunt?.cusines.join(",")}</h4>
+        
+      </div>
     </>
   );
 };
-const styleObj={background:"black", color:"white"}
-const Footer = () => {
+
+const Body = () => {
   return (
     <>
-      <h3 style={styleObj}>Footer</h3>
+      <div className="restrauntlist">
+        {/* {RestrauntCard(restrauntList[0])} */}
+        <RestrauntCard restraunt={restrauntList[0]} hello={"world"} />
+        <RestrauntCard restraunt={restrauntList[1]} />
+        <RestrauntCard restraunt={restrauntList[2]} />
+        <RestrauntCard restraunt={restrauntList[3]} />
+        <RestrauntCard restraunt={restrauntList[4]} />
+        <RestrauntCard restraunt={restrauntList[5]} />
+      </div>
     </>
+  );
+};
+const styleObj = { background: "black", color: "white" };
+const Footer = () => {
+  return (
+    <React.Fragment>
+      <>
+        <h3 style={styleObj}>Footer</h3>
+      </>
+    </React.Fragment>
   );
 };
 
---
+++ Chapter-4/Hw.md
@@ -0,0 +1,2 @@
+Q.1> What is optional chaning?
+A.1> 
